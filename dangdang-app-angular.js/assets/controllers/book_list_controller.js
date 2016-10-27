7ce2c6375863cfc2ba6c8924da992f90b026eb67
yue1121 <dfxgd@mac-5.local>
2016-10-27 12:49:09 +0800

    delete

--- dangdang-app-angular.js/assets/controllers/book_list_controller.js
+++
@@ -1,8 +0,0 @@
-app.controller('ListController', ['$scope', 'commonService', '$routeParams',
-function($scope, c_s, $routeParams) {
-  $scope.data = [];
-  c_s.getData($routeParams.id, function(res) {
-    console.dir(res);
-    $scope.data = res.data;
-  })
-}])
